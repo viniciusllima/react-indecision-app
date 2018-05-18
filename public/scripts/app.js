c271a38908604d185f2367f40e81a728fcaa1cf4
viniciusllima <viniciusllima@gmail.com>
2018-05-18 14:50:47 -0300

    Add public folder

---
+++ public/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+console.log('App.js is running!');
+
+// -- JSX
+var template = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'Indecision App'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'This is some info'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item one'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item two'
+    )
+  )
+);
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
---
+++ src/app.js
@@ -0,0 +1,19 @@
+console.log('App.js is running!');
+
+// -- JSX
+var template = (
+  <div>
+    <h1>Indecision App</h1>
+    <p>This is some info</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
+  </div>
+);
+
+
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
